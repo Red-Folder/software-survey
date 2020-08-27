00fe4a5a379e9a54c671271e107172a43fdfc588
Red Folder <markbryantaylor@yahoo.co.uk>
2020-08-27 11:59:57 +0100

    Changed to use the model from the editcontext

--- SoftwareSurvey/Components/OptionGrid.razor.cs
+++ SoftwareSurvey/Components/OptionGrid.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using SoftwareSurvey.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,15 +8,19 @@ namespace SoftwareSurvey.Components
 {
     public partial class OptionGrid<TValue>
     {
-        [Parameter]
-        public TValue Model { get; set; }
+        [CascadingParameter]
+        public EditContext EditContext { get; set; }
+
         [Parameter]
         public string NotApplicableLabel { get; set; }
 
         private List<Option> _options;
 
+        private TValue Model { get; set; }
+
         protected override void OnParametersSet()
         {
+            Model = (TValue)this.EditContext.Model;
             _options = Model.GetType().GetProperties().Select(x => new Option(this, Model, x)).ToList();
         }
     }
