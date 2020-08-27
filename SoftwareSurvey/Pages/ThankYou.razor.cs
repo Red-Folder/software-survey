60eab37af3db95145866e60cdc5c9e05693ad72d
Red Folder <markbryantaylor@yahoo.co.uk>
2020-08-27 18:50:14 +0100

    Provides retry logic if fails to save

--- SoftwareSurvey/Pages/ThankYou.razor.cs
+++ SoftwareSurvey/Pages/ThankYou.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using SoftwareSurvey.Models;
 using SoftwareSurvey.Services;
+using System;
 using System.Threading.Tasks;
 
 namespace SoftwareSurvey.Pages
@@ -16,7 +17,24 @@ namespace SoftwareSurvey.Pages
         private bool Saved { get; set; }
         private bool Error { get; set; }
 
-        protected override void OnInitialized()
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                SaveData();
+            }
+        }
+
+        private void HandleRetry(EventArgs eventArgs)
+        {
+            Saved = false;
+            Error = false;
+            StateHasChanged();
+
+            SaveData();
+        }
+
+        private void SaveData()
         {
             var task = _persistanceManager.Persist(_surveyResponse);
             task.ContinueWith(async x =>
