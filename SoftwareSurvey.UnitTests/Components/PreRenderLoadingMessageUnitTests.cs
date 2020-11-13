c262d814bfe8f4a28052d1a41ee0a079a36be6a4
Red Folder <markbryantaylor@yahoo.co.uk>
2020-11-13 11:04:42 +0000

    Added tests for the PreRenderLoadingMessage - needed to include a UnitTestMode to allow control over the render cycles

---
+++ SoftwareSurvey.UnitTests/Components/PreRenderLoadingMessageUnitTests.cs
@@ -0,0 +1,58 @@
+﻿using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using SoftwareSurvey.Components;
+using SoftwareSurvey.Services;
+using Xunit;
+
+namespace SoftwareSurvey.UnitTests.Components
+{
+    public class PreRenderLoadingMessageUnitTests
+    {
+        [Fact]
+        public void ShowsLoadingMessage_OnFirstRender()
+        {
+            using var context = new TestContext();
+            context.Services.AddSingleton(new Mock<IEventLoggingService>().Object);
+
+            IRenderedComponent<PreRenderLoadingMessage> cut = context.RenderComponent<PreRenderLoadingMessage>(parameters =>
+                parameters
+                    .Add(p => p.UnitTestMode, true)
+                    .AddChildContent("<div>Hello World</div>")
+            );
+
+            Assert.Contains("Loading ...", cut.Markup);
+        }
+
+        [Fact]
+        public void ShowsChildContent_OnSecondRender()
+        {
+            using var context = new TestContext();
+            context.Services.AddSingleton(new Mock<IEventLoggingService>().Object);
+
+            IRenderedComponent<PreRenderLoadingMessage> cut = context.RenderComponent<PreRenderLoadingMessage>(parameters => 
+                parameters
+                    .Add(p => p.UnitTestMode, true)
+                    .AddChildContent("<div>Hello World</div>")
+            );
+
+            Assert.DoesNotContain("<div>Hello World</div>", cut.Markup);
+
+            cut.Render();
+            Assert.Contains("<div>Hello World</div>", cut.Markup);
+        }
+
+        [Fact]
+        public void ShowsChildContent_IfInDefaultMode()
+        {
+            using var context = new TestContext();
+            context.Services.AddSingleton(new Mock<IEventLoggingService>().Object);
+
+            IRenderedComponent<PreRenderLoadingMessage> cut = context.RenderComponent<PreRenderLoadingMessage>(parameters =>
+                parameters.AddChildContent("<div>Hello World</div>")
+            );
+
+            Assert.Contains("<div>Hello World</div>", cut.Markup);
+        }
+    }
+}
